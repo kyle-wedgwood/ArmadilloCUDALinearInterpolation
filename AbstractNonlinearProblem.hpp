cfad3941a8842a18ffe4784f6f94644d9e532757
kyle-wedgwood <k.c.a.wedgwood@exeter.ac.uk>
2016-08-03 20:38:15 +0100

    uploading abstract problem files

---
+++ AbstractNonlinearProblem.hpp
@@ -0,0 +1,15 @@
+#ifndef ABSTRACTCNONLINEARPROBLEMHEADERDEF
+#define ABSTRACTCNONLINEARPROBLEMHEADERDEF
+
+#include <armadillo>
+
+class AbstractNonlinearProblem
+{
+
+  public:
+
+    virtual void ComputeF( const arma::vec& u, arma::vec& f) = 0;
+
+};
+
+#endif
